425579d4b8febe27eb959b2b069bede19896a0df
natyp <natypaezh@outlook.com>
2024-10-13 21:23:38 -0500

    Ajuste login Empresa

--- app/src/main/java/com/example/worldpetsmobile/LoginBusiness.java
+++ app/src/main/java/com/example/worldpetsmobile/LoginBusiness.java
@@ -21,7 +21,7 @@ public class LoginBusiness extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login_empresa);
 
 
         spinner = findViewById(R.id.spinner);
