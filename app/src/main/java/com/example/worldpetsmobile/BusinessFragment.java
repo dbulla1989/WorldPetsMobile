c8e07f3692718d3183f6f841535781501cf4143f
daniel <danbul1989@gmail.com>
2024-10-13 20:57:34 -0500

    update

--- app/src/main/java/com/example/worldpetsmobile/BusinessFragment.java
+++ app/src/main/java/com/example/worldpetsmobile/BusinessFragment.java
@@ -7,6 +7,11 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,6 +28,7 @@ public class BusinessFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+    private Spinner spinner;
 
     public BusinessFragment() {
         // Required empty public constructor
@@ -52,13 +58,34 @@ public class BusinessFragment extends Fragment {
         if (getArguments() != null) {
             mParam1 = getArguments().getString(ARG_PARAM1);
             mParam2 = getArguments().getString(ARG_PARAM2);
+
         }
     }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        // Inflar el diseño del fragmento
+        View view = inflater.inflate(R.layout.fragment_business, container, false);
+
+        // Usar findViewById en la vista inflada para obtener el Spinner
+        spinner = view.findViewById(R.id.spinner);
+
+        // Cargar las options en el Spinner (puedes agregar las options aquí)
+        List<String> options = new ArrayList<>();
+        options.add("NIT");
+        options.add("RUT");
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<>(getContext(), android.R.layout.simple_spinner_item, options);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        // Devolver la vista inflada
+        return view;
+
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_business, container, false);
+        //return inflater.inflate(R.layout.fragment_business, container, false);
+
+
     }
 }
