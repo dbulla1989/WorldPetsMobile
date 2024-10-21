24f26740b9f8b66a0ee0867af0d74c1dccd1f34a
natyp <natypaezh@outlook.com>
2024-10-20 20:57:40 -0500

    Ajuste login Empresa

--- app/src/main/java/com/example/worldpetsmobile/ui/gallery/GalleryFragment.java
+++ app/src/main/java/com/example/worldpetsmobile/ui/gallery/GalleryFragment.java
@@ -10,7 +10,7 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.lifecycle.ViewModelProvider;
 
-import com.example.worldpetsmobile.databinding.FragmentGalleryBinding;
+  import com.example.worldpetsmobile.databinding.FragmentGalleryBinding;
 
 public class GalleryFragment extends Fragment {
 
